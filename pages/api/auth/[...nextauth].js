b52174b8bcf26d22d17fb35995a654092a9e6f39
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-07-13 13:26:28 +0530

    fix: add `secret` to nextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
 
 export default NextAuth({
+    secret: process.env.SECRET,
     providers: [
         TwitterProvider({
             clientId: process.env.TWITTER_CLIENT_ID,
